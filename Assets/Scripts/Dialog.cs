ccd7ec3898df4a74a35b3850b729623affa273f0
lanza10 <jlanza100@gmail.com>
2023-11-05 16:43:48 +0100

    Diálogos funcionan con mando

--- Assets/Scripts/Dialog.cs
+++ Assets/Scripts/Dialog.cs
@@ -8,6 +8,7 @@ using System;
 public class Dialog : MonoBehaviour
 {
     // Start is called before the first frame update
+    public CharacterControl control;
     private bool isPlayerInRange;
     private Map _playersControl = null;
     public NPCConversation MyConversation;
@@ -16,24 +17,67 @@ public class Dialog : MonoBehaviour
     private void OnEnable()
     {
         _playersControl.Enable();
+        ConversationManager.OnConversationStarted += ConversationStart;
+        ConversationManager.OnConversationEnded += ConversationEnd;
     }
 
     private void OnDisable()
     {
         _playersControl.Disable();
+        ConversationManager.OnConversationStarted -= ConversationStart;
+        ConversationManager.OnConversationEnded -= ConversationEnd;
     }
 
     private void Awake()
     {
         _playersControl = new Map();
         _playersControl.Exploracion.Conversar.performed += Conversar;
+        _playersControl.Conversacion.Confirmar.performed += Confirmar;
+        _playersControl.Conversacion.Siguiente.performed += Siguiente;
+        _playersControl.Conversacion.Anterior.performed += Anterior;
+        _playersControl.Conversacion.Opcion.performed += CambiaOpcion;
        
+
     }
     // Update is called once per frame
     void Update()
     {
     }
+    private void Confirmar(InputAction.CallbackContext context)
+    {
+        if (context.ReadValue<float>() == 1)
+        {
+            ConversationManager.Instance.PressSelectedOption();
+        }
+    }
 
+    private void CambiaOpcion(InputAction.CallbackContext context)
+    {
+        if (context.ReadValue<float>() < 0)
+        {
+            ConversationManager.Instance.SelectNextOption();
+        } 
+        else if (context.ReadValue<float>() > 0)
+        {
+            ConversationManager.Instance.SelectPreviousOption();
+        }
+    }
+
+    private void Siguiente(InputAction.CallbackContext context)
+    {
+        if (context.ReadValue<float>() == 1)
+        {
+            ConversationManager.Instance.SelectNextOption();
+        }
+    }
+
+    private void Anterior(InputAction.CallbackContext context)
+    {
+        if (context.ReadValue<float>() == 1)
+        {
+            ConversationManager.Instance.SelectPreviousOption();
+        }
+    }
     private void Conversar(InputAction.CallbackContext context) {
         if (context.ReadValue<float>() == 1 && isPlayerInRange) {
             
@@ -58,4 +102,15 @@ public class Dialog : MonoBehaviour
             Debug.Log("Salió del colllider");
         }
     }
+
+    private void ConversationStart()
+    {
+        control.DesactivarMap();
+    }
+
+    private void ConversationEnd()
+    {
+        control.ActivarMap();
+    }
+
 }
