fda0b7ac63074688d8b37a98c5470effde327fee
zarlorcode <lorena.zarapo@gmail.com>
2023-11-25 16:57:07 +0100

    Bug de stakOverflow arreglado

--- Assets/Scripts/Revisor.cs
+++ Assets/Scripts/Revisor.cs
@@ -9,13 +9,16 @@ public class Revisor : MonoBehaviour
     enum ESTADOS {QUIETO, CAMINANDO, REVISANDO, ALTERADO  }
     //Stack<ESTADOS> SSstate = new Stack<ESTADOS>();
     private Transform destino;
-    private GameObject Evan;
     private float velocidad = 2;
+
+    private ESTADOS _estado;
     ESTADOS estado // property
     {
-        get { return estado; }
-        set { estado = value; }
+        get { return _estado; }
+        set { _estado = value; }
     }
+
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -42,28 +45,31 @@ public class Revisor : MonoBehaviour
                 }
                 break;
             case ESTADOS.CAMINANDO:
-                
+                //Debug.Log("Estoy Caminando");
                 break;
-            case ESTADOS.REVISANDO:
+           /*ase ESTADOS.REVISANDO:
               
                 break;
             case ESTADOS.ALTERADO:
                 
-                break;
+                break;*/
         }
     }
 
    void desplazamiento() {
         Vector3 direccion = (dialogoAnterior.control.transform.position - transform.position).normalized;
-
+        destino = dialogoAnterior.control.transform;
+        Vector3 nuevaPosicion = new Vector3(destino.position.x + 1.5f, transform.position.y, destino.position.z + 1.5f);
         // Calcula la distancia entre el objeto y el destino
-        float distancia = Vector3.Distance(transform.position, destino.position);
+        //float distancia = Vector3.Distance(transform.position, destino.position);
+        transform.position = Vector3.MoveTowards(transform.position, nuevaPosicion, velocidad * Time.deltaTime);
 
         // Si la distancia es mayor que 0.1, mueve el objeto hacia el destino
+        /*
         if (distancia > 0.1f)
         {
             // Utiliza transform.Translate para mover el objeto en la dirección calculada
             transform.Translate(direccion * velocidad * Time.deltaTime, Space.World);
-        }
+        }*/
     }
 }
