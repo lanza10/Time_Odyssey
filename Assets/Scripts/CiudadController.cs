f4d0d6ebe0a2b9447f1135ca45b3d232967b3073
lanza10 <jlanza100@gmail.com>
2023-12-14 18:33:25 +0100

    ciudad

---
+++ Assets/Scripts/CiudadController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CiudadController : MonoBehaviour
+{
+    private Vector3 posReini = new Vector3(0f, 0f, 400f);
+    private bool estaEnReini = false;
+    public float velocidad = 100f;
+
+    void Start()
+    {
+    }
+
+        // Update is called once per frame
+        void FixedUpdate()
+    {
+        Vector3 direccion = posReini - transform.position;
+        direccion.Normalize();
+        if (!estaEnReini)
+        {
+            transform.position += direccion * velocidad * Time.deltaTime;
+
+            if (Vector3.Distance(transform.position, posReini) < 3f)
+            {
+                estaEnReini = true;
+            }
+        }else
+        {
+            transform.position = new Vector3(0f, 0f, 100f);
+            estaEnReini = false;
+        }
+    }
+}
